1f5b06ee4ac649f245ac6eb933eb02f19337a7da
Sergey <maraev@mail.ru>
2019-07-23 00:18:49 +0300

    add concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,16 @@
 const gulp = require('gulp');
+const concat = require('gulp-concat');
+
+function styles() {
+  return gulp.src('src/css/**/*.css')
+  .pipe(concat('styles.css'))
+  .pipe(gulp.dest('dist/css'));
+}
+
+function scripts() {
+  gulp.src('')
+  .pipe(gulp.dest(''));
+}
+
+gulp.task('styles', styles);
+gulp.task('scripts', styles);
