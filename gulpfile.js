a4f1da9aaea871d6d84efde42874f447346f2d72
Sergey <maraev@mail.ru>
2019-07-23 20:20:41 +0300

    add autoprefixer and fix problem

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,37 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixerCSS = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const del = require('del');
 
 function styles() {
   return gulp.src('src/css/**/*.css')
-  .pipe(concat('styles.css'))
-  .pipe(gulp.dest('dist/css'));
+    .pipe(concat('styles.css'))
+    .pipe(autoprefixerCSS({
+      overrideBrowserslist: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cleanCSS({ level: 2 }))
+    .pipe(gulp.dest('dist/css'));
 }
 
 function scripts() {
-  gulp.src('')
-  .pipe(gulp.dest(''));
+  return gulp.src('src/js/**/*.js')
+    .pipe(uglify({ toplevel: true }))
+    .pipe(gulp.dest('dist/js'));
+}
+
+function watch() {
+  gulp.watch('./src/css/**/*.css', styles);
+  gulp.watch('./src/js/**/*.js', scripts);
+}
+
+function clean() {
+  return del(['dist/*']);
 }
 
 gulp.task('styles', styles);
-gulp.task('scripts', styles);
+gulp.task('scripts', scripts);
+gulp.task('watch', watch);
+gulp.task('clean', clean);
---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+function divide(param1, param2) {
+  return param1 / param2;
+}
+
+console.log(divide(1, 3));
+console.log(divide(1, 53));
