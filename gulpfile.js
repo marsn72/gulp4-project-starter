9c368f1d65386830fead752e70146666c75f93a7
Sergey <maraev@mail.ru>
2019-08-18 16:22:00 +0300

    add notify, autoprefixer, watch

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,23 @@ gulp.task('pug', ()=>{
 
 gulp.task('sass', ()=>{
     return gulp.src('src/static/scss/*.scss')
+    .pipe(gp.sourcemaps.init())
     .pipe(gp.sass())
+    .pipe(gp.autoprefixer({
+        browsers: ['last 5 versions'],
+    }))
+    .on("error", gp.notify.onError({
+        message: "Error: <%= error.message %>",
+        title: "Error compilied styles"
+      }))
     .pipe(gp.csso())
+    .pipe(gp.sourcemaps.write())
     .pipe(gulp.dest('build/css'));
 });
+
+gulp.task('watch', ()=>{
+    gulp.watch('src/pug/**/*.pug', gulp.series('pug'));
+    gulp.watch('src/static/scss/**/*.scss', gulp.series('sass'));
+});
+
+gulp.task('default', gulp.series(gulp.parallel('pug', 'sass'), 'watch'));
