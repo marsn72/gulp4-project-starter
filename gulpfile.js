d3b3baf7ec8389f05d84199cc55cb1b135f8260f
Sergey <maraev@mail.ru>
2019-08-18 17:03:28 +0300

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 const gulp = require('gulp');
 const gp = require('gulp-load-plugins')();
+const browserSync = require('browser-sync').create();
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+});
 
 gulp.task('pug', ()=>{
     return gulp.src('src/pug/pages/*.pug')
     .pipe(gp.pug({
         pretty: true
     }))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build'))
+    .on('end', browserSync.reload);
 });
 
 gulp.task('sass', ()=>{
@@ -22,7 +32,10 @@ gulp.task('sass', ()=>{
       }))
     .pipe(gp.csso())
     .pipe(gp.sourcemaps.write())
-    .pipe(gulp.dest('build/css'));
+    .pipe(gulp.dest('build/css'))
+    .pipe(browserSync.reload({
+        stream: true
+    }));
 });
 
 gulp.task('watch', ()=>{
@@ -30,4 +43,5 @@ gulp.task('watch', ()=>{
     gulp.watch('src/static/scss/**/*.scss', gulp.series('sass'));
 });
 
-gulp.task('default', gulp.series(gulp.parallel('pug', 'sass'), 'watch'));
+gulp.task('default', gulp.series(gulp.parallel('pug', 'sass'), 
+                                 gulp.parallel('watch', 'serve')));
