93e8b8f1e8b55c2195156cad27957a1aea32249e
Sergey <maraev@mail.ru>
2019-08-18 15:33:58 +0300

    add plugin sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp');
-const pug = require('gulp-pug');
+const gp = require('gulp-load-plugins')();
 
 gulp.task('pug', ()=>{
     return gulp.src('src/pug/pages/*.pug')
-    .pipe(pug({
+    .pipe(gp.pug({
         pretty: true
     }))
     .pipe(gulp.dest('build'));
+});
+
+gulp.task('sass', ()=>{
+    return gulp.src('src/static/scss/*.scss')
+    .pipe(gp.sass())
+    .pipe(gp.csso())
+    .pipe(gulp.dest('build/css'));
 });
